7f2b4a3b6d53cd23f79b20644239a0b635708b86
Ilya Lugovoy <lugovoj.ilya.94@gmail.com>
2020-04-15 09:20:59 +0300

    add Cart

--- WebStore_geekbrains/Controllers/HomeController.cs
+++ WebStore_geekbrains/Controllers/HomeController.cs
@@ -27,5 +27,10 @@ namespace WebStore_geekbrains.Controllers
         {
             return View();
         }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
     }
 }
