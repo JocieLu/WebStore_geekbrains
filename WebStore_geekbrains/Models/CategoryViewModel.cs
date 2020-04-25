6a1da511d388d5d14a0234a66d07b65e9a003719
Ilya Lugovoy <lugovoj.ilya.94@gmail.com>
2020-04-25 10:44:20 +0300

    view component categories

---
+++ WebStore_geekbrains/Models/CategoryViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore_geekbrains.Domain.Entities.Base.Interfaces;
+
+namespace WebStore_geekbrains.Models
+{
+    public class CategoryViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        public CategoryViewModel()
+        {
+            ChildCategories = new List<CategoryViewModel>();
+        }
+
+        /// <summary>
+        /// Дочерние секции
+        /// </summary>
+        public List<CategoryViewModel> ChildCategories { get; set; }
+
+        /// <summary>
+        /// Родительская секция
+        /// </summary>
+        public CategoryViewModel ParentCategory { get; set; }
+
+    }
+}
