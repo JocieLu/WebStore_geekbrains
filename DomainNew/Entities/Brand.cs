0694d4d5f86ef024f0e126eef106982fdfe40526
Ilya Lugovoy <lugovoj.ilya.94@gmail.com>
2020-04-26 10:29:05 +0300

    remove old Domains

---
+++ DomainNew/Entities/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore_geekbrains.Domain.Entities.Base.Interfaces;
+
+namespace WebStore_geekbrains.Domain.Entities.Base
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ DomainNew/ProductFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore_geekbrains.Domain
+{
+    public class ProductFilter
+    {
+        public int? CategoryID { get; set; }
+        public int? BrandID { get; set; }
+    }
+}
